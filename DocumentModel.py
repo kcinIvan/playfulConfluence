f69e15175469ccaac8e7593be118a94081792718
Semkin Ivan <ISemkin@sportmaster.ru>
2024-12-26 20:48:52 +0300

    Добавлено определение типа документа по коду документа. Добавлена функция вывода основной информации о странице.

--- DocumentModel.py
+++ DocumentModel.py
@@ -1,9 +1,12 @@
 import connector
 from bs4 import BeautifulSoup
+import pandas as pd
+import io
+import re
 
 
 class DocumentModel:
-    def __init__(self, page_id, keydoc):
+    def __init__(self, page_id):
         confluence = connector.get_connect()
 
         # получение информации о странице по id
@@ -46,14 +49,89 @@ class DocumentModel:
             raise Exception("No tables found on the webpage.")
 
         self.id = page_id  # id страницы
-        self.keyDoc = keydoc  # ключ документа
-        self.title = page["title"] # заголовок страницы
-        self.typeDoc = "non"  # тип документа
-        self.numberDoc = 0  # порядковый номер для типа документа
+        self.keyDoc = None  # ключ документа
+        self.title = page["title"]  # заголовок страницы
+        self.typeDoc = None  # тип документа
+        self.numberDoc = None  # порядковый номер для типа документа
         self.tables = tables  # таблицы на странице
-        self.__macro_replacements = macro_replacements
+        self.macro_replacements = macro_replacements
         self.__link_replacements = link_replacements
         self.__soup = soup
+        self.get_keydoc()
+
+    def get_keydoc(self):
+        # Можно брать только таблицу под индексом 0 игнорируя остальные
+        for table in self.tables:
+
+            # Преобразуем таблицу в DataFrame
+            try:
+                df = pd.read_html(io.StringIO(str(table)))[0]
+
+            except ValueError:
+                # Пропускаем таблицы, которые не удалось преобразовать
+                continue
+
+            try:
+                # Транспонируем таблицу, чтобы строки стали столбцами
+                df = df.transpose()
+
+                # Используем значения первой строки как заголовки столбцов
+                df.columns = df.iloc[0]  # Первая строка становится заголовками
+                df = df.drop(0)  # Удаляем первую строку, так как она теперь заголовок
+
+                # Теперь df содержит транспонированную таблицу с новыми заголовками
+
+                # Ищем таблицу паспорта документа из утверждения, что только она содержит столбцы "Код", "Назначение"
+                codes_column_name = next((col for col in df.columns if "Код" in str(col)), None)
+                if codes_column_name is not None:
+                    # Если такие столбцы есть пробуем получить код документа и проверить его на соответствие шаблону
+                    code_value = df[codes_column_name][1]
+
+                    if pd.isna(code_value):
+                        print(f"Код документа не указан")
+                        break
+                    else:
+                        input_string = self.macro_replacements[code_value]
+
+                        # Шаблон для извлечения значения из <ac:parameter ac:name="key">
+                        match = re.search(r'<ac:parameter ac:name="key">([^<]+)</ac:parameter>', input_string)
+
+                        if match:
+                            value = match.group(1)  # Извлекаем значение вида "API-SAMPLE-001"
+
+                            # Шаблон для проверки и разбора: три группы, разделенные дефисами
+                            # последняя группа только цифры
+                            regex = r'^([A-Za-z]+)-([A-Za-z]+)-(\d{3})$'
+
+                            match_parts = re.match(regex, value)
+
+                            if match_parts:
+                                # Извлекаем три части: первую, вторую и третью
+                                type_part = match_parts.group(1)  # Первая группа (буквы)
+                                key_part = match_parts.group(3).zfill(
+                                    3)  # Третья группа (цифры), добавляем ведущие нули
+
+                                self.keyDoc = match_parts.group(0)
+                                self.typeDoc = type_part
+                                self.numberDoc = key_part
+                                break
+                            else:
+                                print(f'Код документа "{value}" не соответствует шаблону.')
+                                break
+                        else:
+                            print('Не удалось получить код документа.')
+                            break
+            except KeyError:
+                print(f"Не удалось преобразовать таблицу для определения кода документа")
+
+    def print_info(self):
+        print("******************************")
+        print("id страницы: "+str(self.id))
+        print("Ключ документа: "+str(self.keyDoc))
+        print("Заголовок страницы: "+str(self.title))
+        print("Тип документа: "+str(self.typeDoc))
+        print("Порядковый номер для типа документа: "+str(self.numberDoc))
+        print("******************************")
 
     def patch_page(self):
         confluence = connector.get_connect()
@@ -61,7 +139,7 @@ class DocumentModel:
         soup = self.__soup
 
         # Восстанавливаем <ac:structured-macro> в остальной части документа
-        for placeholder, original_html in self.__macro_replacements.items():
+        for placeholder, original_html in self.macro_replacements.items():
             soup = BeautifulSoup(str(soup).replace(placeholder, original_html), "html.parser")
 
         # Восстанавливаем <ac:link> в остальной части документа
--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pageHandler
 urllib3.disable_warnings()
 
 if __name__ == '__main__':
-    page = DocumentModel.DocumentModel(804702898, "DD-MPA-004")
-    pageHandler.add_macros_to_tables_dd(page)
-    page.patch_page()
+    page = DocumentModel.DocumentModel(979503163)
+    # pageHandler.add_macros_to_tables_dd(page)
+    # pageHandler.add_macros_to_tables_intg(page)
+    page.print_info()
+    # page.patch_page()
